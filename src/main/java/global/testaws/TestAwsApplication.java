0c58e17708481b51ba2a60449525f58b8665510b
m1 <symbat.saliamov@alatoo.edu.kg>
2025-10-01 17:41:58 +0600

    qwert

---
+++ src/main/java/global/testaws/TestAwsApplication.java
@@ -0,0 +1,22 @@
+package global.testaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class TestAwsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TestAwsApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String index(){
+        return "index";
+    }
+
+}
---
+++ src/test/java/global/testaws/TestAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package global.testaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
