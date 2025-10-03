d45f1eb4b94adcbbb75b6dd80c5cacfd662affc3
m1 <symbat.saliamov@alatoo.edu.kg>
2025-10-03 17:08:35 +0600

    asdfgh

--- src/main/java/global/testaws/TestAwsApplication.java
+++ src/main/java/global/testaws/TestAwsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @SpringBootApplication
 @Controller
 public class TestAwsApplication {
